0b642e4828a918fc7a8544f6f121d56740e9d8de
A_Naqvi <Ali.Naqvi.Dev@gmail.com>
2023-01-23 00:06:33 -0600

    lilly animations and sprite split

--- Assets/Character2DController.cs
+++ Assets/Character2DController.cs
@@ -12,8 +12,11 @@ public class Character2DController : MonoBehaviour
 
     public Rigidbody2D rigidBody;
 
+    private Camera camera;
+
     private void Start()
     {
+        camera = Camera.main;
     }
 
     private void Update()
@@ -27,17 +30,17 @@ public class Character2DController : MonoBehaviour
         //calculate angle of projectile
         Vector3 mousePosition = Input.mousePosition;
         Vector3 screenPoint =
-            Camera.main.WorldToScreenPoint(transform.localPosition);
+            camera.WorldToScreenPoint(transform.localPosition);
 
         if (mousePosition.x < screenPoint.x)
         {
             transform.localScale = new Vector3(-1.69f, 1.69f, 1f);
-            projectileOrigin.localScale = new Vector3(-0.08f, -0.08f, 1f);
+            projectileOrigin.localScale = new Vector3(-1f, -1f, 0f);
         }
         else
         {
             transform.localScale = new Vector3(1.69f, 1.69f, 1f);
-            projectileOrigin.localScale = new Vector3(0.08f, 0.08f, 1f);
+            projectileOrigin.localScale = new Vector3(1f, 1f, 0f);
         }
 
         //rotate arm
